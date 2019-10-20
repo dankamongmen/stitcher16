695c0c4d0e7756d3a4fe2f1918a22b60a03f262a
nick black <dankamongmen@gmail.com>
2019-10-20 16:26:24 -0400

    stitcher16: parse up argv, add usage()

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,26 @@
 
 // Adapted from https://docs.opencv.org/master/d8/d19/tutorial_stitcher.html
 
-int main(int argc, char** argv) {
-  cv::Stitcher::Mode mode = cv::Stitcher::PANORAMA;
-  std::vector<cv::Mat> imgs;
-  std::string result_name = "output.jpg"; // FIXME
+void usage(const char* name, int ret) {
+  std::cout << "usage: " << name << "output input1 [ input2...]" << std::endl;
+  exit(ret);
+}
 
+int main(int argc, char** argv) {
+  cv::Stitcher::Mode mode = cv::Stitcher::SCANS;
+  std::vector<cv::Mat> imgs;
+
+  if(argc < 3){
+    usage(argv[0], EXIT_FAILURE);
+  }
+  const std::string result_name = argv[1];
+  for(auto arg = argv + 2 ; arg ; ++arg){
+    cv::Mat img = cv::imread(cv::samples::findFile(*arg));
+  }
   cv::Mat pano;
   cv::Ptr<cv::Stitcher> stitcher = cv::Stitcher::create(mode);
   cv::Stitcher::Status status = stitcher->stitch(imgs, pano);
-  if (status != cv::Stitcher::OK)
-  {
+  if(status != cv::Stitcher::OK){
       std::cerr << "Can't stitch images, error code = " << int(status) << std::endl;
       return EXIT_FAILURE;
   }
